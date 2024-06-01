f27511007410c8f7ea1f53317d4abe08282c2b1c
k.santhosh <64588768+santhosh1900@users.noreply.github.com>
2024-06-01 15:12:34 +0530

    Update key.js

--- key.js
+++ key.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI : "mongodb+srv://sandy:Killer007@cluster0.vvcgl.mongodb.net/myFirstDatabase2?retryWrites=true&w=majority"
+    MONGO_URI : "mongodb+srv://sandy:u9yDkaEv4z3A7Sqi@cluster0.5oi1t.mongodb.net/onlineBookBank?retryWrites=true&w=majority&appName=Cluster0",
 };
 
 
